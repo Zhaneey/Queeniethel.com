07ae8bace82665d16ccd24d38caad74bf862dbe3
Zhaneey <114083844+Zhaneey@users.noreply.github.com>
2022-09-21 22:50:31 +0800

    Add files via upload

---
+++ Confession/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Queenie Gwapa",
+    introTitle: 'Hello Love',
+    introDesc: `We've Been talking
+    for almost 2months now
+    and I'm wondering if i can make us two official,
+    so I have a question for you<33`,
+    btnIntro: '^^HiHi^^',
+    title: 'Can I be your Boyfriend? 🥰',
+    desc: 'DI PWEDE MO BALIBAD ',
+    btnYes: 'Yes 😻',
+    btnNo: 'Naur 😒',
+    question: 'In this world with 7 billion people why me? T^T',
+    btnReply: 'Send 🥰',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'I love you so much my Love 😘😘',
+    messDesc: 'Take care po and ayna sig puyat T^T',
+    btnAccept: 'I love you too <3',
+    
+}
